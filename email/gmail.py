a3797d7fd3beb7c06caa5bbd050b5ece482c2222
Mohamed Thoufeeq <thoufeeq.mohd@gmail.com>
2015-10-30 14:35:56 +0530

    Update gmail.py

--- email/gmail.py
+++ email/gmail.py
@@ -4,13 +4,17 @@ g = gmail.login('username@gmail.com', 'password')
 
 g.inbox().mail()
 
-unread = g.inbox().mail(unread=True, sender="thoufeeq.mohd@gmail.com")
+unread_mail = g.inbox().mail(unread=True, sender="thoufeeq.mohd@gmail.com")
 
-unread[0].fetch()
-print unread[0].body
+if not unread_mail:
+        print "Sorry! No new notifications received from the admin."
+else:
+        unread_mail[0].fetch()
+        print "ATTENTION!\n"
+        print unread_mail[0].body
 
 # mark as read
-for i in unread:
-        i.read() 
+for i in unread_mail:
+        i.read()
 
 g.logout()
