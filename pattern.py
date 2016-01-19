e51c8484daa37baaefe67bf8b412451fb99d493b
Mohamed Thoufeeq <thoufeeq.mohd@gmail.com>
2016-01-19 21:29:45 +0530

    edited filename

--- pattern.py
+++ pattern.py
@@ -20,7 +20,7 @@ while True:
 		
 
 def pattern_check():
-	img = cv2.imread('dave.jpg')
+	img = cv2.imread('pic.jpg')
 	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	edges = cv2.Canny(gray,50,150,apertureSize = 3)
 	T_count = 0
