a5cf9ce2f444302f8a7fd1115a0b058e4c873afd
Mohamed Thoufeeq <thoufeeq.mohd@gmail.com>
2016-01-31 23:49:15 +0530

    initial commit - Send mail using JavaMail API

---
+++ noticeboardapp/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.thoufeeq.speechtotext;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText resultedTEXT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultedTEXT = (EditText) findViewById(R.id.fill_words);
+        Button startBtn = (Button) findViewById(R.id.Bmail);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendEmail();
+            }
+        });
+
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+            setSupportActionBar(toolbar);
+
+        }
+
+
+    protected void sendEmail() {
+        Log.i("Send email", "");
+        String[] TO = {"raspberrypinpp@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "New Notification");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, resultedTEXT.getText().toString());
+
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Choose email client"));
+            finish();
+            Log.i("Finished sending email...", "");
+        }
+        catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void onButtonClick(View v)
+    {
+        if(v.getId() == R.id.imageButton)
+        {
+
+            promptSpeechInput();
+        }
+    }
+
+    public void promptSpeechInput() {
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something");
+
+        try {
+            startActivityForResult(i, 100);
+        }
+        catch(ActivityNotFoundException a)
+        {
+            Toast.makeText(MainActivity.this, "Sorry! Your device does not support speech", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onActivityResult(int request_code, int result_code, Intent i)
+    {
+        super.onActivityResult(request_code, result_code, i);
+
+        switch(request_code)
+        {
+            case 100: if(result_code == RESULT_OK && i != null)
+            {
+                ArrayList<String> result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                resultedTEXT.setText(result.get(0));
+
+            }
+                break;
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
