3631661c2493f1144563251454d7fdb5d2514fb4
Mohamed Thoufeeq <thoufeeq.mohd@gmail.com>
2016-02-01 09:39:10 +0530

    Initial commit

---
+++ noticeboardapp/Config.java
@@ -0,0 +1,9 @@
+package com.example.thoufeeq.smartnoticeboard;
+
+/**
+ * Created by Thoufeeq on 31/01/16.
+ */
+public class Config {
+    public static final String EMAIL ="username@gmail.com";
+    public static final String PASSWORD ="supersecurepasswordhere";
+}
